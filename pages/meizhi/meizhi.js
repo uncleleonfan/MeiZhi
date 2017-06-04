ef05ba7c0c44137c3ee91789757a830434598f88
Leon <526247082@qq.com>
2017-06-04 17:59:25 +0800

    Setup Projects

---
+++ pages/meizhi/meizhi.js
@@ -0,0 +1,72 @@
+// meizhi.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    meizhi: []
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      meizhi: ["https://ws1.sinaimg.cn/large/d23c7564ly1fg7ow5jtl9j20pb0pb4gw.jpg",
+        "https://ws1.sinaimg.cn/large/d23c7564ly1fg6qckyqxkj20u00zmaf1.jpg",
+        "https://ws1.sinaimg.cn/large/610dc034ly1fg5dany6uzj20u011iq60.jpg", 
+        "https://ws1.sinaimg.cn/large/610dc034ly1ffyp4g2vwxj20u00tu77b.jpg",
+        "https://ws1.sinaimg.cn/large/610dc034ly1ffxjlvinj5j20u011igri.jpg", "https://ws1.sinaimg.cn/mw690/610dc034ly1ffwb7npldpj20u00u076z.jpg"]
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
